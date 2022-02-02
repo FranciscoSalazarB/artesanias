6b7ffc50874af1c21107c49415c99d041c45a049
ernesto <nepurra@gmail.com>
2022-02-02 11:56:59 -0600

    sistema de validacion de usuarios

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
-})->name('index');
+    return view('welcome');
+});
 
-Route::get('/login', function(){
-    return view('login');
-})->name('login');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
